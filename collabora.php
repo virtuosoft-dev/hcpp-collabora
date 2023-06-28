83f7a33c7a8b8661c6ab83859eac4aa479c9b496
Steveorevo <steve@steveorevo.com>
2023-06-28 11:08:03 -0700

    Add checkbox option

--- collabora.php
+++ collabora.php
@@ -17,10 +17,31 @@ if ( ! class_exists( 'Collabora') ) {
         public function __construct() {
             global $hcpp;
             $hcpp->webdav = $this;
-            $hcpp->add_action( 'priv_unsuspend_domain', [ $this, 'priv_unsuspend_domain' ] );
-            $hcpp->add_action( 'new_web_domain_ready', [ $this, 'new_web_domain_ready' ] );
-            $hcpp->add_action( 'priv_delete_user', [ $this, 'priv_delete_user' ] );
+            // $hcpp->add_action( 'priv_unsuspend_domain', [ $this, 'priv_unsuspend_domain' ] );
+            // $hcpp->add_action( 'new_web_domain_ready', [ $this, 'new_web_domain_ready' ] );
+            // $hcpp->add_action( 'priv_delete_user', [ $this, 'priv_delete_user' ] );
+            $hcpp->add_action( 'render_page', [ $this, 'render_page' ] );
         }
+
+        /**
+         * Inject Collabora Server option under Advanced Options
+         */
+        public function render_page( $args ) {
+            if ( $args['page'] == 'edit_web' ) {
+                global $hcpp;
+                $content = $args['content'];
+                $code = '<div class="form-check u-mb10">
+                    <input class="form-check-input" type="checkbox" name="collabora_support" id="collabora_support">
+                    <label for="collabora_support">Enable Collabora (/coolwsd subfolder)</label>
+                </div>';
+                $before = $hcpp->getLeftMost( $content, '<div x-show="showAdvanced">' ) . '<div x-show="showAdvanced">';
+                $content = $hcpp->delLeftMost( $content, '<div x-show="showAdvanced">' );
+                $content = $before . $code . $content;
+                $args['content'] = $content;
+            }
+            return $args;
+        }
+        
         
         
     }
