33d153991d60a03f8724c1c5487cfc40a83942cc
Steveorevo <steve@steveorevo.com>
2023-06-28 12:32:48 -0700

    wip

--- collabora.php
+++ collabora.php
@@ -20,6 +20,7 @@ if ( ! class_exists( 'Collabora') ) {
             // $hcpp->add_action( 'priv_unsuspend_domain', [ $this, 'priv_unsuspend_domain' ] );
             // $hcpp->add_action( 'new_web_domain_ready', [ $this, 'new_web_domain_ready' ] );
             // $hcpp->add_action( 'priv_delete_user', [ $this, 'priv_delete_user' ] );
+            $hcpp->add_action( 'csrf_verified', [ $this, 'csrf_verified' ] );
             $hcpp->add_action( 'render_page', [ $this, 'render_page' ] );
         }
 
@@ -31,8 +32,8 @@ if ( ! class_exists( 'Collabora') ) {
                 global $hcpp;
                 $content = $args['content'];
                 $code = '<div class="form-check u-mb10">
-                    <input class="form-check-input" type="checkbox" name="collabora_support" id="collabora_support">
-                    <label for="collabora_support">Enable Collabora (/coolwsd subfolder)</label>
+                    <input class="form-check-input" type="checkbox" name="collabora_support" id="collabora_support" checked="true">
+                    <label for="collabora_support">Enable Collabora subfolder (' . $_GET['domain'] . '/coolwsd)</label>
                 </div>';
                 $adv_div = '<div x-cloak x-show="showAdvanced">';
                 $before = $hcpp->getLeftMost( $content, $adv_div ) . $adv_div;
@@ -41,6 +42,14 @@ if ( ! class_exists( 'Collabora') ) {
                 $args['content'] = $content;
             }
             return $args;
+        }
+
+        // Intercept form submission to include Collabora subfolder option
+        public function csrf_verified() {
+            if ( isset( $_REQUEST['domain'] ) ) {
+                global $hcpp;
+                $hcpp->log( $_REQUEST );
+            }
         }
     }
     new Collabora();
